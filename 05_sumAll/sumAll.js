320c14c5176e0f0cc3ad60aff0f7c607918e3eeb
AnazThaj <anaz.thaj@gmail.com>
2022-08-28 18:07:47 +0530

    Complete exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let total = 0; //*starting to adapting the practice of adding ';'
 
+    if (start < 0 || end < 0 || typeof (start) !== 'number' || typeof (end) !== 'number') {
+        return 'ERROR'
+    } else if (start < end) {
+        for (let i = start; i <= end; i++) {
+            total += i;
+        }
+    } else {
+        for (let i = end; i <= start; i++) {
+            total += i;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
