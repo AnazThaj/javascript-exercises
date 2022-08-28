a165b67fe640a45ee7f75bc7ec4930208f5e5743
AnazThaj <anaz.thaj@gmail.com>
2022-08-28 17:50:55 +0530

    Complete exercise 04-removeFromArray (a bit challenging but done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...items) {
+    for (let i = 0; i < items.length; i++) {
+        if (arr.indexOf(items[i]) >= 0) {
+            arr.splice(arr.indexOf(items[i]), 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
