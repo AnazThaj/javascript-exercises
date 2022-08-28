8bf60fe743159c432c338cfcc6dcf4f865fd5f2a
Thaj Anaz <anaz.thaj@gmail.com>
2022-08-28 18:48:12 +0530

    Complete exercise 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+    const celsius = (fahrenheit - 32) * (5 / 9)
+    return Math.round((celsius + Number.EPSILON) * 10) / 10
+    // return celsius
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+    const fahrenheit = (celsius * (9 / 5) + 32)
+    return Math.round((fahrenheit + Number.EPSILON) * 10) / 10
+    // return fahrenheit
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
